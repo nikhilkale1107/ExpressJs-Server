42f89c291faa75bd231efc8aece80a4b40be2692
Nikhil Kale <nikhilkale1107@gmail.com>
2024-03-01 12:31:38 +0530

    Add Product

--- server.js
+++ server.js
@@ -1,14 +1,33 @@
 const http = require('http');
+const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
 
-app.use((req, res, next)=>{
-    console.log("In the middleware");
-    next(); // Allows the request to continue to the next middleware in line
+app.use(bodyParser.urlencoded({extended:false}));
+
+
+// app.use((req, res, next)=>{
+//     console.log("In the middleware");
+//     next(); // Allows the request to continue to the next middleware in line
+// });
+
+// app.use('/',(req, res, next)=>{
+//     console.log("This always runs");
+//     next();
+// });
+
+app.use('/add-product',(req, res, next)=>{
+    // console.log("In another middleware");
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="text" name="title"><button type="submit">Add Product</button></form>');
 });
 
-app.use((req, res, next)=>{
-    console.log("In another middleware");
+app.use('/product',(req,res,next)=>{
+    console.log(req.body)
+    res.redirect('/');
+})
+
+app.use('/',(req, res, next)=>{
+    // console.log("In another middleware");
     res.send('<h1>Hello</h1>');
 });
 
